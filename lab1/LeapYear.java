9b1d0e074c3340d233fdbbdd460b4c92475d16ad
WhoYoung99 <linking2046.cv97@g2.nctu.edu.tw>
2017-01-27 21:22:30 +0800

    change if else order and indent

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -15,17 +15,14 @@ public class LeapYear {
     }
 
     public static boolean isLeapYear(int year) {
-        if ((year % 4) == 0) {
-            if ((year % 100) != 0) {
-                return true;
-                } else {
-                    if ((year % 400) != 0) {
-                        return false;
-                    }
-                return true;
-            }
-        } else {
+        if (year % 4 != 0) {
             return false;
+        } else if (year % 400 == 0) {
+            return true;
+        } else if (year % 100 == 0) {
+            return false;
+        } else {
+            return true;
         }
     }
 
